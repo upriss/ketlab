5d415d8b9f39d250d218c44d0f99f626cea622e4
upriss <upriss@gmail.com>
2022-12-06 20:54:42 +0100

    test

--- exercises11.py
+++ exercises11.py
@@ -1,1 +1,13 @@
-s1 = { x for x in range (-5,5) if x % 3 == 0}
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+restklasse2 = { x for x in range (-5,6) if x % 3 == 0}
+restklasse1 = { x for x in range (-5,6) if x % 3 == 2}
+restklasse3 = { x for x in range (-5,6) if x % 3 == 1}
+Anzahl_Elemente_in_Z3 = 3
+
+def multInv(n,k):
+    return pow(n,-1,k)
+
+def addInv(n,k):
+    return -n%k
