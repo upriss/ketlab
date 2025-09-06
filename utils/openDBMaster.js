f68ab050f1727f8873aa1144bcd25838258efa8f
upriss <upriss@gmail.com>
2025-09-06 08:46:35 +0200

    update

--- utils/openDBMaster.js
+++
@@ -1,6 +0,0 @@
-const sqlPromise = initSqlJs({
-    locateFile: file => `https://cdn.jsdelivr.net/gh/upriss/ketlab/utils/${file}` });
-const dataPromise = fetch("https://cdn.jsdelivr.net/gh/upriss/ketlab/masterR.db").then(res => res.arrayBuffer());
-const [SQL, buf] = await Promise.all([sqlPromise, dataPromise])
-const db = new SQL.Database(new Uint8Array(buf));
-export {db}
